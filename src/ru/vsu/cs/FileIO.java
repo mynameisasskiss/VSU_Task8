5061845647e16e396915d7cf4fc217f9bb250453
Anthony <avamkakoedelovich@ya.ru>
2024-11-11 12:17:00 +0300

    run via terminal: java ru.vsu.vs.NoGUI <input file> <output file>

--- src/ru/vsu/cs/FileIO.java
+++ src/ru/vsu/cs/FileIO.java
@@ -35,4 +35,9 @@ public class FileIO {
             }
         }
     }
+    public static void writeStringToFile(String fileName, String str) throws IOException {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
+            writer.write(str);
+        }
+    }
 }
--- src/ru/vsu/cs/GUI.java
+++ src/ru/vsu/cs/GUI.java
@@ -1,44 +1,71 @@
 package ru.vsu.cs;
 
 import javax.swing.*;
-import javax.swing.table.TableColumn;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-import javax.swing.table.DefaultTableModel;
 import java.util.Arrays;
-import java.util.List;
-import java.util.ArrayList;
 
 public class GUI extends JFrame {
     private JButton inputButton;
     private JPanel pane;
     private JTextArea textArea1;
     private JTextArea textArea2;
-
+    private JButton saveAsButton;
+    private JTextField outputFileField;
+    private JTextField errorField;
     public GUI() {
         super("GUI");
         setContentPane(pane);
         setVisible(true);
+        //outputFileField.setVisible(false);
+        //errorField.setVisible(false);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setSize(1200, 1000);
+        setSize(800, 600);
 
         inputButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                String data[] = textArea1.getText().split("\n");
-                int[][] intArr = ArrayConverter.convertToIntArray(data);
-                int[][] outputData = Evaluate.getFriendlyCount(intArr);
-                for (int i = 0; i < outputData.length; i++) {
-                    textArea2.append(Arrays.toString(outputData[i]).replace("[", "").replace("]", "").replace(",", "") + "\n");
+                if (textArea1.getText().contains(".")) {
+                    try {
+                        int[][] intArr = FileIO.readIntMatrixFromFile(textArea1.getText());
+                        int[][] outputData = Evaluate.getFriendlyCount(intArr);
+                        for (int i = 0; i < outputData.length; i++) {
+                            textArea2.append(Arrays.toString(outputData[i]).replace("[", "").replace("]", "").replace(",", "") + "\n");
+                        }
+                    } catch (Exception ex) {
+                        errorField.setVisible(true);
+                        errorField.setText("Ошибка при чтении файла");
+                    }
+
+                } else {
+                    String data[] = textArea1.getText().split("\n");
+                    int[][] intArr = ArrayConverter.convertToIntArray(data);
+                    int[][] outputData = Evaluate.getFriendlyCount(intArr);
+                    textArea2.setText(Arrays.toString(outputData).replace("[", "").replace("]", "").replace(",", "") + "\n");
                 }
 
+
+            }
+        });
+        saveAsButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                outputFileField.setVisible(true);
+                if (textArea2.getText() != "") {
+                    System.out.println(textArea2.getText());
+                    try {
+                        FileIO.writeStringToFile(outputFileField.getText(), textArea2.getText());
+                    } catch (Exception ex) {
+                        errorField.setVisible(true);
+                        errorField.setText("Ошибка при записи файла");
+                    }
+                }
             }
         });
     }
 
     public static void main(String[] args) {
         GUI v = new GUI();
-        v.setSize(400, 400);
         v.setTitle("Дружелюбная программа");
     }
 }
